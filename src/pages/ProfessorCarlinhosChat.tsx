537d3f68345b305537fdc5321740a7fed76fc7a9
[dyad] <carloslirau@gmail.com>
2025-10-20 19:59:14 -0300

    [dyad] Corrigindo completamente o alinhamento da sidebar com o conteúdo principal - wrote 2 file(s)

--- src/pages/ProfessorCarlinhosChat.tsx
+++ src/pages/ProfessorCarlinhosChat.tsx
@@ -160,7 +160,7 @@ const ProfessorCarlinhosChat = () => {
       <div className="lg:pl-64">
         {/* Header */}
         <header className="bg-white shadow-sm border-b border-gray-200">
-          <div className="flex items-center justify-between h-16 px-4 sm:px:6 lg:px-8">
+          <div className="flex items-center justify-between h-16 px-4 sm:px-6 lg:px-8">
             {/* Mobile menu button */}
             <div className="lg:hidden">
               <Button
