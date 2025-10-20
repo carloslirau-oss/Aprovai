6b55cc5743bdb9725071193f1357bb33db6009fb
[dyad] <carloslirau@gmail.com>
2025-10-20 20:08:18 -0300

    [dyad] Corrigindo layout para sidebar e conteúdo ficarem lado a lado usando flexbox - wrote 2 file(s)

--- src/pages/ProfessorCarlinhosChat.tsx
+++ src/pages/ProfessorCarlinhosChat.tsx
@@ -149,7 +149,7 @@ const ProfessorCarlinhosChat = () => {
   };
 
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="min-h-screen bg-gray-50 flex">
       {/* Sidebar */}
       <Sidebar 
         isOpen={sidebarOpen} 
@@ -157,7 +157,7 @@ const ProfessorCarlinhosChat = () => {
       />
 
       {/* Main Content */}
-      <div className="lg:pl-64">
+      <div className="flex-1 flex flex-col">
         {/* Header */}
         <header className="bg-white shadow-sm border-b border-gray-200">
           <div className="flex items-center justify-between h-16 px-4 sm:px-6 lg:px-8">
@@ -200,7 +200,7 @@ const ProfessorCarlinhosChat = () => {
         </header>
 
         {/* Main Content */}
-        <main className="flex h-[calc(100vh-4rem)]">
+        <main className="flex-1 flex flex-col">
           {/* Chat Container */}
           <div className="flex-1 flex flex-col">
             {/* Chat Header */}
