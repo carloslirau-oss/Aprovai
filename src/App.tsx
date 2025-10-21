4c59f817835437f5867f82e650aee266242dbb29
[dyad] <carloslirau@gmail.com>
2025-10-20 22:47:05 -0300

    [dyad] Removendo importação do Register.tsx do App.tsx - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider } from "@/contexts/AuthContext";
 import Login from "./pages/Login";
-import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 import CorretorRedacao from "./pages/CorretorRedacao";
 import ProfessorCarlinhosChat from "./pages/ProfessorCarlinhosChat";
@@ -23,7 +22,6 @@ const App = () => (
           <Routes>
             <Route path="/" element={<Navigate to="/login" replace />} />
             <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/redacoes" element={<CorretorRedacao />} />
             <Route path="/professor-carlinhos" element={<ProfessorCarlinhosChat />} />
